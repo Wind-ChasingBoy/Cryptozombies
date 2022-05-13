bda320c95f7ee772f07e17e1a8a95d5e74b4a1c4
Wind-ChasingBoy <1875444130@qq.com>
2022-05-13 17:10:39 +0800

    Updates

---
+++ precache-manifest.edfa9dd2e374d7f77797daccc115d656.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c5c7590f51152948e89e55e4c46fc98",
+    "url": "/Cryptozombies/index.html"
+  },
+  {
+    "revision": "3a8ec7f993ccec6bfe9e",
+    "url": "/Cryptozombies/static/css/main.714fab77.chunk.css"
+  },
+  {
+    "revision": "823927bb6f131dd1222b",
+    "url": "/Cryptozombies/static/js/2.fffcb7ef.chunk.js"
+  },
+  {
+    "revision": "f20c56f63688e2f8fe92e4d05aec4eb8",
+    "url": "/Cryptozombies/static/js/2.fffcb7ef.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3a8ec7f993ccec6bfe9e",
+    "url": "/Cryptozombies/static/js/main.ad14726d.chunk.js"
+  },
+  {
+    "revision": "079e5ec2ffd670f42d9a",
+    "url": "/Cryptozombies/static/js/runtime-main.049da485.js"
+  },
+  {
+    "revision": "d2b2559b651e6a50f8d43f206dcdb8e7",
+    "url": "/Cryptozombies/static/media/Btn_Register_Normal.d2b2559b.png"
+  },
+  {
+    "revision": "d3684ace4f10c62c9209b45d3bb724c2",
+    "url": "/Cryptozombies/static/media/Btn_Register_Over.d3684ace.png"
+  },
+  {
+    "revision": "64a6f16c4f5ef7e189e75dfc0a52a25f",
+    "url": "/Cryptozombies/static/media/homecard@2x.64a6f16c.png"
+  },
+  {
+    "revision": "9d0437bbb5e705bad95ac929361faf3e",
+    "url": "/Cryptozombies/static/media/index_bg.9d0437bb.png"
+  },
+  {
+    "revision": "41236031fc933b053717ef1760601e36",
+    "url": "/Cryptozombies/static/media/start_btn.41236031.png"
+  },
+  {
+    "revision": "2a4a2235f6ed0e838b49a3f1bec4aae1",
+    "url": "/Cryptozombies/static/media/tester-bg@2x.2a4a2235.png"
+  },
+  {
+    "revision": "b102b8003aab95865cb7363428273a16",
+    "url": "/Cryptozombies/static/media/walls.b102b800.jpg"
+  },
+  {
+    "revision": "08087132d6dcfcd5c5f471880c104712",
+    "url": "/Cryptozombies/static/media/zombierun.08087132.png"
+  }
+]);
